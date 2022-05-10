d2bb242c84bdeeb1c1ac3b165c624361fe1ddc2d
royastik27 <royastik27@gmail.com>
2022-05-10 12:31:02 +0600

    Modular Pattern - Bug Fixing

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -17,7 +17,7 @@ const controller = (function()
 
     const validateItem = function(item)
     {
-        if(!item.caseAmountmount && !item.quantity)
+        if(!item.caseAmount && !item.quantity)
         {
             DOM.DOMelements.errorEl.textContent = 'Invalid Input!';
             return false;
@@ -47,11 +47,13 @@ const controller = (function()
         // CALCULATING
         let price = 0;
 
-        if(DOMitem.caseamount)  price += item.casePrice * DOMitem.caseamount;
+        if(DOMitem.caseAmount)  price += item.casePrice * DOMitem.caseAmount;
 
         if(DOMitem.quantity) price += item.price * DOMitem.quantity;
 
         if(DOMitem.retail) price = item.mrp * DOMitem.quantity;
+
+        // console.log(price);
 
         return {
             serialNo: ++itemNumber,
@@ -67,7 +69,7 @@ const controller = (function()
         getItemNumber: () => itemNumber,
         init: function(){
             totalPrice = itemNumber = 0;            
-            DOM.clearTable();            
+            DOM.clearTable();
         },
         addItem: async function() {
             const DOMitem = DOM.getItem();
@@ -114,6 +116,8 @@ const DOM = (function()
     const DOMstationary = document.getElementById('stationary');
     const DOMretail = document.getElementById('retail');
 
+    const DOMcustomerName = document.getElementById('customer-name');
+
     return {
         DOMelements: {
             table: DOMtable,
@@ -153,104 +157,21 @@ const DOM = (function()
             SLNO.innerHTML = item.serialNo;
             PRODUCTNAME.innerHTML = item.productName;
 
-            if(item.caseamount)  CASEAMOUNT.innerHTML = item.caseAmount;
+            if(item.caseAmount)  CASEAMOUNT.innerHTML = item.caseAmount;
             if(item.quantity)    QUANTITY.innerHTML = item.quantity;    
             
             PRICE.innerHTML = item.price;
             
-            TOTALPRICE.innerHTML = item.price;
+            TOTALPRICE.innerHTML = item.totalPrice;
         }
     }
 })();
 
-// TESTING
-controller.init();
-
-async function getItem(productName)
-{      
-    let options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 
-                'application/json;charset=utf-8'
-        },
-        body: JSON.stringify({ productName: productName })
-    }
-
-    const res = await(await fetch("/productsapi", options)).json();
-
-    return res;
-}
-
-async function addItem()
-{
-    // GET ITEM VALUES FROM DOM
-    const productName = document.getElementById('product-name').value;
-    let caseamount = parseInt(document.getElementById('caseamount').value);
-    let quantity = parseInt(document.getElementById('quantity').value);
-    const stationary = document.getElementById('stationary').checked;
-    const retail = document.getElementById('retail').checked;
-
-    if(isNaN(caseamount)) caseamount = 0;
-    if(isNaN(quantity)) quantity = 0;
-
-    // GETTING DATA
-    const res = await getItem(productName);
-
-    // ERROR HANDLING
-    const errorEl = document.getElementById('show-error');
-    if(!caseamount && !quantity)
-    {
-        errorEl.textContent = 'Invalid Input!';
-        return;
-    }
-    else if(!res.success)
-    {
-        errorEl.textContent = res.message;
-        return;
-    }
-    errorEl.textContent = '';
-
-    // SETTING DATA
-    const item = res.data;
-
-    // CALCULATING
-    let price = 0;
-
-    if(caseamount)  price += item.casePrice * caseamount;
-
-    if(quantity) price += item.price * quantity;
-
-    if(retail) price = item.mrp * quantity;
-
-    // UPDATING DOM
-    const el = document.getElementById('product-list');
-
-    var row = el.insertRow(++itemNumber);
-
-    // TABLE CELL SERIAL
-    var SLNO = row.insertCell(0);
-    var PRODUCTNAME = row.insertCell(1);
-    var CASEAMOUNT = row.insertCell(2);
-    var QUANTITY = row.insertCell(3);    
-    var PRICE = row.insertCell(4);
-    var TOTALPRICE = row.insertCell(5);
-
-    // CELL CONTENTS
-    SLNO.innerHTML = itemNumber;
-    PRODUCTNAME.innerHTML = item.productName;
-
-    if(caseamount)  CASEAMOUNT.innerHTML = caseamount;
-    if(quantity)    QUANTITY.innerHTML = quantity;    
-    
-    if(stationary)  PRICE.innerHTML = item.price;
-    else PRICE.innerHTML = item.mrp;
-    
-    TOTALPRICE.innerHTML = price;
-}
-
 document.getElementById('input').addEventListener('keypress', function(event) {
 
-    if (event.key === "Enter")
-        addItem();  // CHANGE to controller.addItem()
+    if (event.key === "Enter" && event.target.nodeName !== "BUTTON")
+        controller.addItem();
 });
+
+// USING
+controller.init();
